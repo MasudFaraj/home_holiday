b6b92971f2730fcbfa20bf678f79a0c81e1bf7f2
MasudFaraj <127840161+MasudFaraj@users.noreply.github.com>
2024-06-21 14:05:05 +0200

    Add files via upload

---
+++ src/main/java/org/cammbs_up/holidayhome/HolidayhomeApplication.java
@@ -0,0 +1,13 @@
+package org.cammbs_up.holidayhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cammbs_up/holidayhome/HolidayhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cammbs_up.holidayhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
