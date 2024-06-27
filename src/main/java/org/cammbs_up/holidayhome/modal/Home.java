af4bd8b89fd1b03ff7ff1f6cac6ea086aa04c6fd
masudfaraj <masudfaraj81@hotmail.com>
2024-06-27 18:27:22 +0200

    class home is created

---
+++ src/main/java/org/cammbs_up/holidayhome/modal/Home.java
@@ -0,0 +1,19 @@
+package org.cammbs_up.holidayhome.modal;
+
+/**
+ * Home
+ * Dokumentation
+ * validation
+ * input
+ * logic
+ * output
+ * Verbesserung der Strukturierung
+ * Entfernung von Redundanzen
+ *
+ * @author Masoud Faraj
+ * @version 27.06.2024
+ */
+public class Home {
+
+
+}
